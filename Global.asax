e392043baff9ae87123f707181672b0c7201a0bc
Divya-Yengampeta <33324502+Divya-Yengampeta@users.noreply.github.com>
2018-01-29 15:40:24 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWith_Entity.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWith_Entity.Startup))]
+namespace MVCWith_Entity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
